cfa41b4e42ac013c4846dbfb5e2a46e6fd089eeb
Guylian Bollon <guylianbollon@hotmail.com>
2021-05-08 20:15:22 +0200

    laatste commits

---
+++ FAITH/ImageLoader.swift
@@ -0,0 +1,8 @@
+//
+//  ImageLoader.swift
+//  FAITH
+//
+//  Created by Guylian Bollon on 08/05/2021.
+//
+
+import Foundation
---
+++ FAITH/ScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  ScreenView.swift
+//  FAITH
+//
+//  Created by Guylian Bollon on 08/05/2021.
+//
+
+import SwiftUI
+
+struct ScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScreenView()
+    }
+}
